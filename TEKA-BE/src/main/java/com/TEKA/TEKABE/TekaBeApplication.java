437522d538c693ba27b8b6e91e5203a08bc216fb
ARHL2023 <andyholguinluna@gmail.com>
2024-04-27 19:57:05 -0500

    Creación de proyecto TEKA-BE

---
+++ TEKA-BE/src/main/java/com/TEKA/TEKABE/TekaBeApplication.java
@@ -0,0 +1,13 @@
+package com.TEKA.TEKABE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TekaBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TekaBeApplication.class, args);
+	}
+
+}
---
+++ TEKA-BE/src/test/java/com/TEKA/TEKABE/TekaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TEKA.TEKABE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TekaBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
